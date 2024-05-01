b2af461febb51231c92e4b516f54fab61c46e53e
github_Scorpion945 <maxx.vasin05@mail.ru>
2024-05-01 14:20:03 +0400

    add plus

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Headers;
+using pr3;
+
+namespace pr3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите А:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите B:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите операцию:");
+            char c = Convert.ToChar(Console.ReadLine());
+            Class1 clas = new Class1();
+            switch (c)
+            {
+                case '+':
+                    Console.WriteLine($"a + b = {clas.Plus(a, b)}");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
